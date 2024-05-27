f86bc1cf299095db336bb950ee451ba2ffd4a274
Ayodeji4074-cloud <adeniyiemmanuel998@gmail.com>
2024-05-27 06:08:13 -0700

    page

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,9 +1,11 @@
-
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination
 """
+Deletion-resilient hypermedia pagination
+"""
+
 import csv
-from typing import Dict, List
+import math
+from typing import List, Dict
 
 
 class Server:
@@ -12,8 +14,6 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initializes a new Server instance.
-        """
         self.__dataset = None
         self.__indexed_dataset = None
 
@@ -36,4 +36,30 @@ class Server:
             truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Takes 2 integer arguments and returns a dictionary with
+        the following key-value pairs:
+            index: index of the first item in the current page
+            next_index: index of the first item in the next page
+            page_size: the current page size
+            data: actual page of the dataset
+        Args:
+            index(int): first required index
+            page_size(int): required number of records per page
+        """
+        dataset = self.indexed_dataset()
+        data_length = len(dataset)
+        assert 0 <= index < data_length
+        response = {}
+        data = []
+        response['index'] = index
+        for i in range(page_size):
+            while True:
+                curr = dataset.get(index)
+                index += 1
+                if curr is not None:
 
--- 0x00-pagination/README.md
+++ 0x00-pagination/README.md
@@ -1,1 +1,2 @@
-0-simple_helper_function.py
+0x00-pagination
+
